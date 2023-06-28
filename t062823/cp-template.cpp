6f9bded4c22895d89b5e79f5c8397c87a3a91bea
thieu-le <thieu.le569@gmail.com>
2023-06-28 14:54:04 +0700

    first commit

---
+++ t062823/cp-template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    for(int i=0; i<=10; i++) {
+        ;
+    }
+}
